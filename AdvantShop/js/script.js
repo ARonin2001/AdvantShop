e4c72e71843cd310f0ad9b472e9eea38656247be
Alex <Aronin.2001@gmail.com>
2021-06-15 17:21:50 +0400

    catalog tovars

--- AdvantShop/js/script.js
+++ AdvantShop/js/script.js
@@ -1,18 +1,37 @@
-let catalogBurger = document.querySelector('.header__catalog-btn'),
-    catalogNav = document.querySelector('.header__catalog-nav');
+'use strict'
+
+let catalogBurger = document.querySelector('.aside__catalog-btn'),
+    catalogNav = document.querySelector('.aside__catalog-nav');
 
 catalogBurger.addEventListener('click', function() {
-    if(catalogNav.style.height === "100%") {
-        for (let i = 100; i != -1; i--) {
-            console.log("s");
-            catalogNav.style.height = i + "%";
-        }
-        catalogNav.style.border = "none";
+    // if(catalogNav.style.height === "100%") {
+    //     for (let i = 100; i != -1; i--) {
+    //         catalogNav.style.height = i + "%";
+    //     }
+    //     catalogNav.style.border = "none";
+    //     return;
+    // }
+
+    // catalogNav.style.border = "1px solid #efeff4;";
+    // for (let i = 1; i <= 100; i++) {
+    //     catalogNav.style.height = i + "%";
+    // }
+
+    if(catalogNav.style.display === "none") {
+        catalogNav.style.display = "block";
         return;
     }
 
-    catalogNav.style.border = "1px solid #efeff4;";
-    for (let i = 1; i <= 100; i++) {
-        catalogNav.style.height = i + "%";
-    }
+    catalogNav.style.display = "none";
+});
+
+// scroll main baner
+$('.autoplay').slick({
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+    // infinite: true, 
+    // autoplay: true,
+    // autoplaySpeed: 2000,
 });
